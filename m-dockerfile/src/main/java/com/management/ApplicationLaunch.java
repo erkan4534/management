67af71483b3694c2e757db00ca5f025631a317dc
Erkan Yıldırım <erkanyildirim28@gmail.com>
2023-10-28 19:07:55 +0300

    add class and docker

--- m-dockerfile/src/main/java/com/management/ApplicationLaunch.java
+++ m-dockerfile/src/main/java/com/management/ApplicationLaunch.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApplicationLaunch {
-
     public static void main(String[] args) {
         SpringApplication.run(ApplicationLaunch.class, args);
     }
-
 }
