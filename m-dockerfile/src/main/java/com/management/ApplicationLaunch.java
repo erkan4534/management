d9ea8157beafee34ceb22877413ba613c00464cf
Erkan Yıldırım <erkanyildirim28@gmail.com>
2023-10-28 11:41:02 +0300

    add m-dockerfile

---
+++ m-dockerfile/src/main/java/com/management/ApplicationLaunch.java
@@ -0,0 +1,13 @@
+package com.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationLaunch {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationLaunch.class, args);
+    }
+
+}
---
+++ m-dockerfile/src/test/java/com/management/ApplicationLaunchTests.java
@@ -0,0 +1,13 @@
+package com.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationLaunchTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
