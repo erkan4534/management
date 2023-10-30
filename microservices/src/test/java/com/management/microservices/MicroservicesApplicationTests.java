87994c9f5d758f677e8c4d07176ab4e067b2c66f
Erkan Yıldırım <erkanyildirim28@gmail.com>
2023-10-30 18:24:27 +0300

    add m-dockerfile

---
+++ microservices/src/test/java/com/management/microservices/MicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
