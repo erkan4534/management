29342dab4f1a05c9cac9ea2afa57dc639a33e3c3
Erkan Yıldırım <erkanyildirim28@gmail.com>
2023-11-04 21:42:07 +0300

    add microservices

---
+++ microservices/school-management/src/main/java/com/school/management/SchoolManagementApplication.java
@@ -0,0 +1,13 @@
+package com.school.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolManagementApplication.class, args);
+    }
+
+}
