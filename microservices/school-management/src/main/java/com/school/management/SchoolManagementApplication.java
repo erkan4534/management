55c6b3c6828d3a84e33f91aeeb94c5b4d924ca27
Erkan Yıldırım <erkanyildirim28@gmail.com>
2023-11-04 21:54:02 +0300

    add school-management microservices

--- microservices/school-management/src/main/java/com/school/management/SchoolManagementApplication.java
+++ microservices/school-management/src/main/java/com/school/management/SchoolManagementApplication.java
@@ -2,12 +2,12 @@ package com.school.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SchoolManagementApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SchoolManagementApplication.class, args);
     }
-
 }
