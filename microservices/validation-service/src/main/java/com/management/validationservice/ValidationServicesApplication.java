06156dcf6ab10461e8a32f2865a9add109d3eaaf
Erkan Yıldırım <erkanyildirim28@gmail.com>
2023-11-06 18:48:41 +0300

    add notification-service microservices

--- microservices/validation-service/src/main/java/com/management/validationservice/ValidationServicesApplication.java
+++ microservices/validation-service/src/main/java/com/management/validationservice/ValidationServicesApplication.java
@@ -1,4 +1,4 @@
-package com.management.validationservices;
+package com.management.validationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
