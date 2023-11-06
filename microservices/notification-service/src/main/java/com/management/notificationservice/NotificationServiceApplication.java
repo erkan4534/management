6bb9fa666528ddf385337fecf4fc3c586375bd5c
Erkan Yıldırım <erkanyildirim28@gmail.com>
2023-11-06 19:00:24 +0300

    add notification-service microservices

--- microservices/notification-service/src/main/java/com/management/notificationservice/NotificationServiceApplication.java
+++ microservices/notification-service/src/main/java/com/management/notificationservice/NotificationServiceApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class NotificationServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
     }
-
 }
--- microservices/payment-service/src/main/java/com/management/paymentservice/PaymentServiceApplication.java
+++ microservices/payment-service/src/main/java/com/management/paymentservice/PaymentServiceApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class PaymentServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PaymentServiceApplication.class, args);
     }
-
 }
