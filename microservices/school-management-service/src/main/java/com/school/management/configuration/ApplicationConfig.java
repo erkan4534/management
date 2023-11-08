1098331ad72cb156746e272ab4e5fab89831ec87
Erkan Yıldırım <erkanyildirim28@gmail.com>
2023-11-08 17:58:31 +0300

    add RestTemplate in configuration

--- microservices/school-management-service/src/main/java/com/school/management/configuration/ApplicationConfig.java
+++ microservices/school-management-service/src/main/java/com/school/management/configuration/ApplicationConfig.java
@@ -6,8 +6,10 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @OpenAPIDefinition
 @Configuration
@@ -28,6 +30,11 @@ public class ApplicationConfig {
                 .info(new Info()
                         .title("School Management OpenAPI Docs")
                         .version("1.0.0").description("Doc Description"));
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
 
 }
