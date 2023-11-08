ce449f386172edbc16773e7bba41fada8b871b3e
Erkan Yıldırım <erkanyildirim28@gmail.com>
2023-11-08 19:34:40 +0300

    add RestTemplate in configuration

--- microservices/payment-service/src/main/java/com/management/paymentservice/model/PaymentSalary.java
+++ microservices/payment-service/src/main/java/com/management/paymentservice/model/PaymentSalary.java
@@ -6,8 +6,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import java.time.LocalDate;
 
 @Getter
