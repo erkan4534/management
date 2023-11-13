e449e8934fb89f305c6dde0db28913c452559c6e
Erkan Yıldırım <erkanyildirim28@gmail.com>
2023-11-13 19:59:17 +0300

    add notification class

--- microservices/clients/src/main/java/com/clients/notification/NotificationClient.java
+++ microservices/clients/src/main/java/com/clients/notification/NotificationClient.java
@@ -1,6 +1,6 @@
 package com.clients.notification;
 
-
+import com.clients.notification.dto.NotificationDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
