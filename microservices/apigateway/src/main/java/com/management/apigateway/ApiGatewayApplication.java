ae0fd1a160fd838ac3140134c25cf44cd21a780f
Erkan Yıldırım <erkanyildirim28@gmail.com>
2024-02-04 20:20:55 +0300

    add apigateway

--- microservices/apigateway/src/main/java/com/management/apigateway/ApiGatewayApplication.java
+++ microservices/apigateway/src/main/java/com/management/apigateway/ApiGatewayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApigatewayApplication {
+public class ApiGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApigatewayApplication.class, args);
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
 }
