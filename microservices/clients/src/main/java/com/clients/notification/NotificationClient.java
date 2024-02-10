d24b2695901fee9b84dd8bd2d0883e89a216d24d
Erkan Yıldırım <erkanyildirim28@gmail.com>
2024-02-10 18:05:11 +0300

    add rabbitmq

--- microservices/clients/src/main/java/com/clients/notification/NotificationClient.java
+++ microservices/clients/src/main/java/com/clients/notification/NotificationClient.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.RequestBody;
 @FeignClient("notification-service")
 public interface NotificationClient {
     @PostMapping("/notifications")
-    public void sendNotification(@RequestBody NotificationDto request);
+    void sendNotification(@RequestBody NotificationDto request);
 }
