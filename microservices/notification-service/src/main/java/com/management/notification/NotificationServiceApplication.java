6cdc23f7e274838b8af607b32e69a023c054d8b5
Erkan Yıldırım <erkanyildirim28@gmail.com>
2024-02-11 15:46:44 +0300

    add rabbitmq

--- microservices/notification-service/src/main/java/com/management/notification/NotificationServiceApplication.java
+++ microservices/notification-service/src/main/java/com/management/notification/NotificationServiceApplication.java
@@ -1,5 +1,6 @@
 package com.management.notification;
 
+import com.management.notification.model.Notification;
 import com.management.rabbitmq.RabbitMQMessageProducer;
 import com.management.notification.configuration.NotificationConfig;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import java.time.LocalDateTime;
 
 @EnableFeignClients(basePackages = "com.clients.notification")
 @EnableDiscoveryClient
@@ -28,6 +31,12 @@ public class NotificationServiceApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        producer.publish("Test Test",config.getNotificationExchange(),config.getNotificationRoutingKey());
+        //producer.publish("Test Test",config.getNotificationExchange(),config.getNotificationRoutingKey());
+        producer.publish(Notification.builder()
+                        .toCustomerPhone("1111111")
+                        .sender("aaa")
+                        .sentAt(LocalDateTime.now())
+                        .build()
+                ,config.getNotificationExchange(),config.getNotificationRoutingKey());
     }
 }
